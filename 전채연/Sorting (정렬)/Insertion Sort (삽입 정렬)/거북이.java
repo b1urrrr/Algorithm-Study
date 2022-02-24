0d6d0bd228bfe43fd60f6b69f9fc39c34e2977db
b1urrrr <b1urrr@naver.com>
2022-02-25 02:32:12 +0900

    [ADD] 거북이 풀이 업로드(저장)

--- 전채연/Sorting (정렬)/Insertion Sort (삽입 정렬)/거북이.java
+++ 전채연/Sorting (정렬)/Insertion Sort (삽입 정렬)/거북이.java
@@ -1,13 +1,20 @@
+/*
+    백준 2959번 : 거북이
+    - 풀이 유형 : 삽입 정렬 (Insertion Sort)
+*/
+
 import java.util.Scanner;
 
 public class 거북이 {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
-        int[] steps = new int[4];
+        int[] steps = new int[4]; // 걸음의 수 배열
+        // 네 정수 입력
         for (int i = 0; i < 4; i++) {
             steps[i] = stdIn.nextInt(); 
         }
 
+        // 삽입 정렬
         for(int i = 1; i < 4; i++) {
             int temp = steps[i];
             int j = i - 1;
@@ -18,6 +25,7 @@ public class 거북이 {
             steps[j + 1] = temp;
         }
 
-        System.out.println(steps[0] * steps[2]);
+        // 가장 큰 직사각형은 (두 번째로 가장 큰 값) * (네 번째로 가장 큰 값)이므로 이를 출력
+        System.out.println(steps[2] * steps[0]);
     } 
 }
