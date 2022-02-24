7d76b4acd55e23bc6ae1d67c56e1b3fc11ec2be4
b1urrrr <b1urrr@naver.com>
2022-02-25 02:32:48 +0900

    [CORRECT] 줄바꿈 문자 추가

--- 전채연/Sorting (정렬)/Insertion Sort (삽입 정렬)/콘테스트.java
+++ 전채연/Sorting (정렬)/Insertion Sort (삽입 정렬)/콘테스트.java
@@ -1,7 +1,14 @@
+/*
+    백준 5576번 : 콘테스트
+    - 풀이 유형 : 삽입 정렬 (Insertion Sort)
+*/
+
 import java.util.Scanner;
 
-public class 세수정렬 {
+public class 콘테스트 {
+    // 배열의 최댓값 3개의 합을 반환하는 함수
     static int max3(int[] scores, int count) {
+        // 삽입 정렬
         for (int i = 1; i < count; i++) {
             int temp = scores[i];
             int j = i - 1;
@@ -11,18 +18,23 @@ public class 세수정렬 {
             }
             scores[j + 1] = temp;
         }
+        // 최댓값 3개의 합 반환
         return scores[count - 1] + scores[count - 2] + scores[count - 3];
     }
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
-        int[] W = new int[10];
-        int[] K = new int[10];
+        int[] W = new int[10]; // W 대학의 득점 배열
+        int[] K = new int[10]; // K 대학의 득점 배열
+        // W 대학 득점 입력
         for (int i = 0; i < 10; i++) {
             W[i] = stdIn.nextInt();
         }
+        // K 대학 득점 입력
         for (int i = 0; i < 10; i++) {
             K[i] = stdIn.nextInt();
         }
+        
+        // 출력
         System.out.printf("%d ", max3(W, 10));
         System.out.print(max3(K, 10));
     }
