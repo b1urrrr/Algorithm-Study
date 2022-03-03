05cde5898737895c4a25443a8f638cca83b3b381
b1urrrr <b1urrr@naver.com>
2022-03-04 06:26:53 +0900

    [ADD] 교재 3단원 예제 풀이

---
+++ 전채연/교재 예제/자료구조와 함께 배우는 알고리즘 입문/chap03/SeqSearch.java
@@ -0,0 +1,34 @@
+package chap03;
+import java.util.Scanner;
+
+public class SeqSearch {
+    static int seqSearch(int[] a, int n, int key) {
+        int i = 0;
+
+        while (true) {
+            if (i==n) return -1;
+            if (a[i] == key) return i;
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.printf("x[%d] : ", i);
+            x[i] = stdIn.nextInt();
+        }
+
+        System.out.print("검색할 값 : ");
+        int ky = stdIn.nextInt();
+        int idx = seqSearch(x, num, ky);
+
+        if (idx == -1) System.out.println("그 값의 요소가 없습니다.");
+        else System.out.printf("%d은(는) x[%d]에 있습니다.\n", ky, idx);
+    }
+}
---
+++ 전채연/교재 예제/자료구조와 함께 배우는 알고리즘 입문/chap03/SeqSearchFor.java
@@ -0,0 +1,30 @@
+package chap03;
+import java.util.Scanner;
+
+public class SeqSearchFor {
+    static int seqSearch(int[] a, int n, int key) {
+        for (int i = 0; i < n; i++)
+            if(a[i] == key) return i;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.printf("x[%d] : ", i);
+            x[i] = stdIn.nextInt();
+        }
+
+        System.out.print("검색할 값 : ");
+        int ky = stdIn.nextInt();
+        int idx = seqSearch(x, num, ky);
+
+        if (idx == -1) System.out.println("그 값의 요소가 없습니다.");
+        else System.out.printf("%d은(는) x[%d]에 있습니다.\n", ky, idx);
+    }    
+}
