7237da407250c730d81e6cacd1cd6763a533bb69
b1urrrr <b1urrr@naver.com>
2022-03-09 01:53:43 +0900

    [MOD] 에라토스테네스의 채 알고리즘 적용

--- 전채연/Mathematics (수학)/소수_구하기.java
+++ 전채연/Mathematics (수학)/소수_구하기.java
@@ -1,39 +1,34 @@
 /*
     백준 1929번 : 소수 구하기
-    - 문제 유형 : 수학(에라토스테네스의 채 참고)
-    - 풀이 유형 : n의 제곱근 이하의 어떤 소수로도 나누어떨어지지 않는 수 n
+    - 문제 유형 : 수학, 에라토스테네스의 채
 */
 
 import java.util.Scanner;
 
 public class 소수_구하기 {
-    static final int INF = 1000000; // 입력의 최댓값
+    static boolean[] isNotPrime; // 소수가 아님을 표시하는 배열
+
+    // 에라토스테네스의 채 함수
+    static void seive_of_eratosthenes(int range) {
+        // 0과 1은 소수가 아님을 표시
+		isNotPrime[0] = isNotPrime[1] = true;
+        // 2부터 i가 소수이면 자신의 배수는 소수가 아님을 표시
+		for (int i = 2; i * i <= range; i++)
+				if (!isNotPrime[i])
+						for (int j = i * i; j <= range; j += i)
+								isNotPrime[j] = true;
+}
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         int start = stdIn.nextInt(); // M 입력
         int end = stdIn.nextInt(); // N 입력
-        int count = 0; // 발견한 소수의 개수
-        int[] prime = new int[INF]; // 소수를 저장할 배열
+        isNotPrime = new boolean[end + 1];
         
-        // 가장 작은 소수인 2 저장 및 조건 만족 시 출력
-        prime[count++] = 2;
-        if (start <= 2) System.out.println(2);
+        // 에라토스테네스의 채 함수 호출
+        seive_of_eratosthenes(end);
 
-        // 3 이상 end 이하의 수에 대하여 소수 여부 검사
-        for (int i = 3; i <= end; i += 2) {
-            boolean isPrime = true;
-            // 자신의 제곱근 이하의 어떤 소수로도 나누어떨어지지 않음
-            for (int j = 0; prime[j] * prime[j] <= i; j++) {
-                if (i % prime[j] == 0) {
-                    isPrime = false;
-                    break;
-                }  
-            }
-            // 소수 발견 시 저장 및 조건 만족 시 출력
-            if (isPrime) {
-                prime[count++] = i;
-                if (start <= i) System.out.println(i);
-            }
-        }
+        // 범위 내의 소수 출력
+        for (int i = start; i <= end; i++)
+            if (!isNotPrime[i]) System.out.println(i);
     }
 }
