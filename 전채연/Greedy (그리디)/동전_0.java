f322fa6ed6fbd3b8cadcccfe5a4c3a7171b70d7a
b1urrrr <b1urrr@naver.com>
2022-03-18 20:54:30 +0900

    [ADD] 주석 추가

--- 전채연/Greedy (그리디)/동전_0.java
+++ 전채연/Greedy (그리디)/동전_0.java
@@ -1,3 +1,8 @@
+/*
+    백준 11047번 : 동전 0
+    - 문제 유형 : 그리디 알고리즘
+*/
+
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -6,18 +11,19 @@ public class 동전_0 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         StringTokenizer st = new StringTokenizer(s);
-        int num = Integer.parseInt(st.nextToken());
-        int cost = Integer.parseInt(st.nextToken());
+        int num = Integer.parseInt(st.nextToken()); // 동전의 개수 입력
+        int cost = Integer.parseInt(st.nextToken()); // 목표 값 입력
         int[] coins = new int[num];
 
         for (int i = num - 1; i >= 0; i--) {
-            coins[i] = Integer.parseInt(br.readLine());
+            coins[i] = Integer.parseInt(br.readLine()); // 동전의 값어치 입력
         }
         br.close();
 
         int count = 0;
-        while (cost > 0) {
+        while (cost > 0) { // 값이 0이 될 때까지 반복
             for (int c : coins) {
+                // 값어치가 큰 동전부터 사용
                 if (cost >= c) {
                     cost -= c;
                     count ++;
@@ -26,6 +32,7 @@ public class 동전_0 {
             }
         }
 
+        // 동전의 개수 출력
         System.out.println(count);
     }
 }
