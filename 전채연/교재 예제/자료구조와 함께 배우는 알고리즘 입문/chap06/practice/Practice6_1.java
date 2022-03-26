56065567472b52b0561b7d5d565530cd989af25e
b1urrrr <b1urrr@naver.com>
2022-03-26 23:43:06 +0900

    [ADD] 교재 6단원 예제 추가

---
+++ 전채연/교재 예제/자료구조와 함께 배우는 알고리즘 입문/chap06/practice/Practice6_1.java
@@ -0,0 +1,38 @@
+package chap06.practice;
+
+import java.util.Scanner;
+
+public class Practice6_1 {
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    static void bubbleSort(int[] a, int n) {
+        for (int i = n - 1; i > 0; i--) 
+            for (int j = 0; j < i; j++)
+                if (a[j] > a[j + 1])
+                    swap(a, j, j + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("버블 정렬(버전 1)");
+        System.out.print("요솟수 : ");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.printf("x[%d] : ", i);
+            x[i] = stdIn.nextInt();
+        }
+
+        bubbleSort(x, nx);
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        for (int i = 0; i < nx; i++)
+            System.out.printf("x[%d] = %d\n", i, x[i]);
+    }
+}
