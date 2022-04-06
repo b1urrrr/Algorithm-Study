6826ba2682eeffd41ba86d86ce6b84fc0ced853c
kinglim00 <tldtodthd52@naver.com>
2022-04-07 00:15:35 +0900

    [MOD] 7주차 백준 브루트포스 2231번 코드 추가

--- 임서희/백준/7주차/2231번.py
+++ 임서희/백준/7주차/2231번.py
@@ -1,3 +1,4 @@
+# 맞는 내 코드: 1200ms
 N = int(input())
 min_result = N
 
@@ -14,3 +15,19 @@ if min_result != N:
     print(min_result)
 else:
     print(0)
+
+
+# 다른 방법: 1916ms
+n = int(input())
+f = []
+
+for i in range(n):
+    count = 0
+    for i2 in list(str(i)):
+        count += int(i2)
+    if n == i+count:
+        f.append(i)
+if len(f) == 0:
+    print(0)
+else:            
+    print(min(f))
