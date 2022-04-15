372c211cf55f7065f8cdf59dada1f2daa588cfc3
kinglim00 <tldtodthd52@naver.com>
2022-04-15 23:55:24 +0900

    [MOD] 8주차 백준 15650 코드 수정

--- 임서희/백준/8주차/15650번.py
+++ 임서희/백준/8주차/15650번.py
@@ -1,11 +1,11 @@
 N, M = map(int, input().split())
 s = []
 
-def dfs():
+def dfs(start):
     if len(s) == M:
         print(' '.join(map(str,s)))
         return
-    for i in range(1, n+1):
+    for i in range(start, n+1):
         if i not in s:
             s.append(i)
             dfs(i+1)
