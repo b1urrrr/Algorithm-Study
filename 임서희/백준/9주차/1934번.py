2f2e187fbb50687a7f3453d9b2c2da8111ce7080
kinglim00 <tldtodthd52@naver.com>
2022-04-23 00:21:34 +0900

    [MOD] 1934번 유클리드 코드 및 설명 추가

--- 임서희/백준/9주차/1934번.py
+++ 임서희/백준/9주차/1934번.py
@@ -61,3 +61,54 @@ for _ in range(T):
         print(A)
     else:
         print(func(A,B))
+
+
+# 유클리드 알고리즘 이용하기
+# 유클리드 알고리즘이란?
+# 두 수 사이에 존재하는 최대공약수(GCD: greatest common divisor)를 구하는 알고리즘
+
+# 원리: a,b에 대해서(a>b) a를 b로 나눈 나머지를 r이라고 할 때, a와 b의 최대 공약수는 b와 r의 최대공약수와 같다.
+# 임의의 값 a,b가 주어졌을 때 큰 값이 a라고 가정해보자.
+# a를 b로 나눈 나머지가 n이라면 (a%b=n)
+# n이 0이 될 때, b가 최대 공약수이다.
+# n이 0이 아니라면, a에 b값을 다시 넣고 n을 b에 대입한 후 반복한다.
+
+# 반복문
+# 1.
+def gcd(a,b):
+    r = -1
+    while b>0:
+        r = a%b
+        a = b
+        b = r
+    return a
+
+# 2. 더 짧게
+def gcd(a,b): 
+    while a%b!=0: # a%b!=0이라서 return b (나머지가 0 되기 직전 값 return)
+        a,b=b,a%b
+    return b # 만약 b!=0이면 return a (나머지가 0이 된 값이 b가 되었으므로 a return)
+
+# 재귀 호출
+def gcd(a,b):
+    # 파이썬 미지원 삼항 연산자
+    # return b ? gcd(b,a%b):a
+    # [조건] ? [true] : [false]
+    
+    # 파이썬 지원 삼항 연산자
+    # [true] if [조건] else [false]
+    return gcd(b,a%b) if b!=0 else a
+
+
+# 따라서 유클리드 호제법을 이용한 코드
+T = int(input())
+
+for _ in range(T):
+    a,b= map(int, input().split())
+    aa,bb = a, b
+
+    while bb != 0:
+        aa,bb=bb,aa%bb
+    
+    print(a*b//aa) # 입력 값들을 곱하고 GCD로 나누면 최소 공배수 구하기
+    # 입력 값 한 쪽에서 최대 공약수를 약분하게 되므로 
