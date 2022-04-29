b71c7ec77de8c35e5f38deb64404a47fa87521eb
MinYeongPark <yingo24655@naver.com>
2022-04-29 22:54:23 +0900

    [ADD] 백준 2747, 2748번 풀이

---
+++ 박민영/Dynamic programming (동적프로그래밍)/백준/n2748/Main.java
@@ -0,0 +1,27 @@
+/*
+ * @author Minyeong Park
+ * @date 2022.04.29.
+ * 피보나치 수 2
+ * 문제 링크 : https://www.acmicpc.net/problem/2748
+ */
+
+import java.io.*;
+
+public class Main {
+    static long[] fibo_arr;
+    static void fibo(int num) {
+        for (int i = 2; i < num; i++) {
+            fibo_arr[i] = fibo_arr[i - 1] + fibo_arr[i - 2];
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        fibo_arr = new long[n+1];
+
+        fibo_arr[0] = 0;
+        fibo_arr[1] = 1;
+        fibo(n+1);
+        System.out.println(fibo_arr[n]);
+    }
+}
