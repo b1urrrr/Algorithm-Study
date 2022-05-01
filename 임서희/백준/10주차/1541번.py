958ae90ad1f915731a7800818f379bcd1b50104c
kinglim00 <tldtodthd52@naver.com>
2022-05-01 23:38:56 +0900

    [ADD] 10주차 백준 문제 1541번

---
+++ 임서희/백준/10주차/1541번.py
@@ -0,0 +1,14 @@
+a = input().split('-')
+num = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
+
+# 다시 풀기
