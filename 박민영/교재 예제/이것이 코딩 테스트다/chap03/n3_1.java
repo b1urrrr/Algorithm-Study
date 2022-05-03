5ea1673e892f94e0b72be2b090dadaefa22abb7c
MinYeongPark <yingo24655@naver.com>
2022-05-03 19:49:48 +0900

    [ADD] 교재 chap03 1,2

---
+++ 박민영/교재 예제/이것이 코딩 테스트다/chap03/n3_1.java
@@ -0,0 +1,16 @@
+public class n3_1 {
+    public static void main(String[] args) {
+        int n = 1260;
+        int cnt = 0;
+        // 큰 단위의 화폐부터 차례대로 확인
+        int[] cointTypes = {500, 100, 50, 10};
+
+        for (int i = 0; i < 4; i++) {
+            int coin = cointTypes[i];
+            cnt += n / coin;
+            n %= coin;
+        }
+
+        System.out.println(cnt);
+    }
+}
