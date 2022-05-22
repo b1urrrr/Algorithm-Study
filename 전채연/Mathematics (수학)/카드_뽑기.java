a3acb8727cecd7920b27f78c00d029976066762d
b1urrrr <b1urrr@naver.com>
2022-05-22 23:21:19 +0900

    [ADD] 13주차 알고리즘 문제 풀이 업로드

---
+++ 전채연/Mathematics (수학)/카드_뽑기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 카드_뽑기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cards = Integer.parseInt(st.nextToken());
+        int frontO = Integer.parseInt(st.nextToken());
+        int frontX = cards - frontO;
+        int backO = Integer.parseInt(st.nextToken());
+        int backX = cards - backO;
+        int count = Math.min(frontO, backO) + Math.min(frontX, backX);
+        System.out.println(count);
+    }
+}
+
