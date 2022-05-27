b6444c662a348c3d374878af456653d5219e7c97
kinglim00 <tldtodthd52@naver.com>
2022-05-28 05:00:09 +0900

    [ADD] 이코테 교재 및 백준 DFS 문제

---
+++ 임서희/교재/이것이 취업을 위한 코딩 테스트다/3_DFSnBFS/9_음료수 얼려 먹기.py
@@ -0,0 +1,29 @@
+# 음료수 얼려 먹기
+# dfs 사용
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result) # 정답 출력
