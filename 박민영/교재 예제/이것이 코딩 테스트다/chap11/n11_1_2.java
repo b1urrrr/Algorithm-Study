ad3d336df3c6fbd15c872007b683f01ff14ac099
MinYeongPark <yingo24655@naver.com>
2022-07-04 23:59:13 +0900

    [ADD] 교재 11단원 Q1 모험가 길드 풀이

---
+++ 박민영/교재 예제/이것이 코딩 테스트다/chap11/n11_1_2.java
@@ -0,0 +1,34 @@
+// chap11 Q1. 모험가 길드 (해설 코드)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class n11_1_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arrayList.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(arrayList);
+
+        int result = 0; // 총 그룹의 수
+        int count = 0; // 현재 그룹에 포함된 모험가의 수
+
+        for (int i = 0; i < n; i++) { // 공포도를 낮은 것부터 하나씩 확인하며
+            count += 1; // 현재 그룹에 해당 모험가를 포함시키기
+            if (count >= arrayList.get(i)) { // 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
+                result += 1; // 총 그룹의 수 증가시키기
+                count = 0; // 현재 그룹에 포함된 모험가의 수 초기화
+            }
+        }
+        System.out.println(result);
+    }
+}
