34b70dd1f0972a28696c6bfd0d3729a5fd476814
kinglim00 <tldtodthd52@naver.com>
2022-08-05 15:38:17 +0900

    [MOD] 24주차 백준 1120번 코드 추가

--- 임서희/백준/24주차/1120번.py
+++ 임서희/백준/24주차/1120번.py
@@ -15,3 +15,16 @@ while cnt < len(b)-len(a):
     cnt += 1
 print(res)
 print(min(res))
+
+# 범위 수정
+a, b = input().split()
+
+answer = []
+for i in range(len(b) - len(a) + 1):
+    count = 0
+    for j in range(len(a)):
+        if a[j] != b[i + j]:
+            count += 1
+    answer.append(count)
+
+print(min(answer))
