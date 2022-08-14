a1094fb97fe0c76dd09c8d1b98695326dd697907
b1urrrr <b1urrr@naver.com>
2022-08-14 23:55:46 +0900

    [ADD] 크로아티아 알파벳 풀이 업로드

---
+++ 전채연/Implementation (구현)/크로아티아_알파벳.java
@@ -0,0 +1,31 @@
+/*
+    백준 2941번 : 크로아티아 알파벳
+    - 문제 유형 : 구현, 문자열
+*/
+
+import java.util.Scanner;
+
+public class 크로아티아_알파벳 {
+    public static void main(String[] main) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next(); // 단어 입력
+        int answer = word.length();
+
+        for (int i = 0; i < word.length() - 1; i++) {
+            if (word.charAt(i) == 'z' && word.charAt(i+1) == '=') answer--;
+            if (word.charAt(i) == 'c' && word.charAt(i+1) == '=') answer--; 
+            if (word.charAt(i) == 'c' && word.charAt(i+1) == '-') answer--; 
+            if (word.charAt(i) == 'd' && word.charAt(i+1) == '-') answer--; 
+            if (word.charAt(i) == 'l' && word.charAt(i+1) == 'j') answer--; 
+            if (word.charAt(i) == 'n' && word.charAt(i+1) == 'j') answer--; 
+            if (word.charAt(i) == 's' && word.charAt(i+1) == '=') answer--; 
+        }
+
+        for (int i = 0; i < word.length() - 2; i++) {
+            if (word.charAt(i) == 'd' && word.charAt(i + 1) == 'z' && word.charAt(i + 2) == '=') answer--;
+        }
+        
+        // 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력
+        System.out.print(answer);
+    }
+}
