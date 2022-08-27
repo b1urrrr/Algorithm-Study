429e554f9ed1fdb1586e7dd302b70c15e7aaa0a0
kinglim00 <tldtodthd52@naver.com>
2022-08-27 22:38:45 +0900

    [ADD] 백준 27주차 백트래킹 문제 풀이

--- 임서희/백준/27주차/15654번.py
+++ 임서희/백준/27주차/15654번.py
@@ -3,4 +3,20 @@
 
 # 백트래킹이 잘 기억 안나서 N과 M(1)~(4) 다시 복습
 
+# 복습 후 문제 풀이
+# N과 M(1) 참고
 n, m = map(int, input().split())
+n_list = list(map(int, input().split()))
+n_list = sorted(n_list)
+su = []
+
+def dfs():
+    if len(su) == m:
+        print(' '.join(map(str,su)))
+        return
+    for i in n_list:
+        if i not in su:
+            su.append(i)
+            dfs()
+            su.pop()
+dfs()
