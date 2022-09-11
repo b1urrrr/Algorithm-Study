e740b938c76b4b27e5095e8abc9e0e6f8eb77e19
kinglim00 <tldtodthd52@naver.com>
2022-09-11 21:52:09 +0900

    [MOD] 29주차 19949번 코드 추가

--- 임서희/백준/29주차/19949번.py
+++ 임서희/백준/29주차/19949번.py
@@ -13,3 +13,26 @@ def dfs():
         dfs()
         su.pop()
 
+# 다시 풀기
+# 다른 풀이
+def dfs(depth):
+    global cnt
+    if depth == 10:
+        s = 0
+        for j in range(10):
+            if li[j] == ans[j]:
+                s += 1
+        if s >= 5:
+            cnt += 1
+        return 
+    for i in range(1, 6):
+        if depth > 1 and li[depth-2] == li[depth-1] == i:
+            continue
+        li.append(i)
+        dfs(depth+1)
+        li.pop()
+        
+ans = list(map(int, input().split()))
+li, cnt = [], 0
+dfs(0)
+print(cnt)
