0fe8d96c46cd69cf7e00afdc02736e1e142fa97d
kinglim00 <tldtodthd52@naver.com>
2022-09-19 22:54:11 +0900

    [MOD] 31주차 백준 1371번 풀이 추가

--- 임서희/백준/31주차/1371번.py
+++ 임서희/백준/31주차/1371번.py
@@ -18,3 +18,20 @@ res = ''
 for i in max_list:
     res += i
 print(res)
+
+import sys
+input = sys.stdin.read
+
+s = input().replace("\n","").replace(" ","")
+c = [0] * 26
+for i in s:
+    c[ord(i)-97]+=1
+    
+maxx = max(c)
+r = []
+for i in range(len(c)):
+    if c[i] == maxx:
+        r.append(chr(i+97))
+        
+r.sort()
+print(*r,sep="")
