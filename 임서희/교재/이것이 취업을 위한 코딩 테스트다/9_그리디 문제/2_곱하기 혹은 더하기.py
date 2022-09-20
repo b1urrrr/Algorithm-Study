b8e0b04beefb598bdaa1fbaa3e74b015f9461f05
kinglim00 <tldtodthd52@naver.com>
2022-09-20 21:28:40 +0900

    [ADD] 이코테 교재 그리디 기출 문제

--- 임서희/교재/이것이 취업을 위한 코딩 테스트다/9_그리디 문제/2_곱하기 혹은 더하기.py
+++ 임서희/교재/이것이 취업을 위한 코딩 테스트다/9_그리디 문제/2_곱하기 혹은 더하기.py
@@ -10,3 +10,18 @@ for i in n:
 		res *= i
 
 print(res)
+
+# 답안 예시
+n = int(input())
+data = list(map(int, input().split()))
+
+result = 0
+
+count = 0 
+
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count =0
+print(result)
--- 임서희/교재/이것이 취업을 위한 코딩 테스트다/9_그리디 문제/3_문자열 뒤집기.py
+++ 임서희/교재/이것이 취업을 위한 코딩 테스트다/9_그리디 문제/3_문자열 뒤집기.py
@@ -17,3 +17,21 @@ len_0 = len(list_0)
 len_1 = len(list_1)
 
 print(min(len_0,len_1))
+
+# 답안 예시
+data = input()
+count0 = 0
+count1 = 0
+
+if data[0] == '1':
+    count0 += 1
+else:
+    count1 += 1
+
+for i in range(len(data) - 1):
+    if data[i]!=data[i+1]:
+        if data[i+1] == '1':
+            count0 += 1
+        else:
+            count1 += 1
+print(min(count0, count1))
