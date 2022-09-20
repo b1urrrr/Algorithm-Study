e7d27d5b864e09c13a6dc714dbdf159260924519
MinYeongPark <yingo24655@naver.com>
2022-09-20 20:42:10 +0900

    [ADD] 교재 chap12 Q9 마저 ~ Q12 풀이

--- 박민영/교재 예제/이것이 코딩 테스트다/chap12/n12_9.java
+++ 박민영/교재 예제/이것이 코딩 테스트다/chap12/n12_9.java
@@ -1,30 +1,43 @@
-// 진행 중
+// chap12 Q9. 문자열 압축
+
 class Solution {
     public int solution(String s) {
         int answer = s.length();
-        
+
         int index = 0;
-        for (int length = 1; length < s.length() / 2; length++) {
-            int temp_count = 0;
-            while (index <= 5) {
-                String a_str = s.substring(index, index+length);
-                String b_str = s.substring(index+length, index+length*2);
-                System.out.println(a_str + " : " + b_str);
-                
-                if (a_str.equals(b_str)) {
-                    temp_count += length + 1;
-                    index += length * 2;
-                } else {
-                    index += length;
-                    temp_count += length;
+        for (int length = 1; length < s.length() / 2 + 1; length++) {
+            String compressed = "";
+            String prev = s.substring(index, index+length);
+            int cnt = 1;
+
+            // 단위 크기(length) 만큼 증가시키며 이전 문자열과 비교
+            for (int j = length; j < s.length(); j += length) {
+                // 이전 상태와 동일하다면 압축 횟수(cnt) 증가
+                String sub = "";
+                for (int k = j; k < j + length; k++) {
+                    if (k < s.length()) sub += s.charAt(k);
+                }
+
+                if (prev.equals(sub)) cnt += 1;
+                    // 다른 문자열이 나왔다면 (더이상 압축하지 못하는 경우라면)
+                else {
+                    compressed += (cnt >= 2) ? cnt + prev : prev;
+                    sub = "";
+                    // length만큼 추출
+                    for (int k = j; k < j + length; k++) {
+                        if (k < s.length()) sub += s.charAt(k);
+                    }
+                    prev = sub; // 다시 상태 초기화
+                    cnt = 1;
                 }
             }
-            
-            if (temp_count < answer) {
-                answer = temp_count;
-            }
+
+            // 남아 있는 문자열에 대해서 처리
+            compressed += (cnt >= 2) ? cnt + prev : prev;
+
+            // 만들어지는 압축 문자열이 가장 짧은 것이 정답
+            answer = Math.min(answer, compressed.length());
         }
-        
         return answer;
     }
 }
