3a1ef94344bcff41c268f83f1c9d3bd7c30220f2
kinglim00 <tldtodthd52@naver.com>
2022-10-22 22:52:49 +0900

    [MOD] 35주차 백준 9946번 코드 추가

--- 임서희/백준/35주차/9946번.py
+++ 임서희/백준/35주차/9946번.py
@@ -17,3 +17,17 @@ while True:
         res = 'same'
     print('Case ' + str(k) + ': ' + res)
     k += 1
+
+# 위에 내 코드가 틀려서 다른 코드
+# ord로 아스키코드로 변환해야 할 듯
+cnt=1
+while 1:
+    a=input()
+    b=input()
+    if a=='END' and b=='END' :break
+    tmpa,tmpb=[0]*26, [0]*26
+    for i in a: tmpa[ord(i)-97]+=1
+    for i in b: tmpb[ord(i) - 97] += 1
+    if tmpa==tmpb: print("Case %d: same"%cnt)
+    else: print("Case %d: different"%cnt)
+    cnt+=1
