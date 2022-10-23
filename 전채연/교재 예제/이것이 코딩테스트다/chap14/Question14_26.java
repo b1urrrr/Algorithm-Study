a21bcfe94aa7fb6bd6a1063279dce770446e27d0
b1urrrr <b1urrr@naver.com>
2022-10-23 22:14:21 +0900

    [ADD] 이코테 14단원 예제 풀이

--- 전채연/교재 예제/이것이 코딩테스트다/chap14/Question14_26.java
+++ 전채연/교재 예제/이것이 코딩테스트다/chap14/Question14_26.java
@@ -6,26 +6,24 @@ import java.util.*;
 public class Question14_26 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 카드 묶음의 개수 N 입력
-        int[] cards = new int[N];
 
-        for (int i = 0 ; i < N; i++) {
-            cards[i] = Integer.parseInt(br.readLine()); // 카드 묶음의 크기 입력
+        int N = Integer.parseInt(br.readLine()); // 숫자 카드 묶음의 개수 N 입력
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) que.add(Integer.parseInt(br.readLine())); // 숫자 카드 묶음의 크기 입력
+
+        int result = 0;
+        // 카드 묶음이 하나가 될 때까지 가장 작은 숫자 카드 묶음 2개를 합치기
+        while (que.size() > 1) {
+            int a = que.remove();
+            int b = que.remove();
+            int sum = a + b;
+
+            result += sum;
+            que.add(sum);
         }
 
-        if (N == 1) {
-            System.out.println(cards[0]);
-            return ;
-        }
-
-        Arrays.sort(cards);
-
-        int result = cards[0] + cards[1];
-
-        for (int i = 2; i < N; i++) {
-            result += result + cards[i];
-        }
-
+        // 최소 비교 횟수 출력
         System.out.println(result);
     }
 }
