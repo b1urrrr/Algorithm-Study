6b79847d8008a0e0c64cbd0d827279efaef11bdc
MinYeongPark <yingo24655@naver.com>
2022-10-29 10:49:53 +0900

    [MOD] 백준 11576번 풀이 (완료)

--- 박민영/Implement (구현)/백준/n11576/Main.java
+++ 박민영/Implement (구현)/백준/n11576/Main.java
@@ -2,10 +2,10 @@
  * @author Minyeong Park
  * @date 2022.10.28.
  * Base Conversion
+ * 반례 참고 : https://www.acmicpc.net/board/view/91446
  * 문제 링크 : https://www.acmicpc.net/problem/11576
  */
 
-// 현재 진행 중이라서 완료된 코드는 아님...
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,14 +22,19 @@ public class Main {
         int m = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
-        int num = 0;
+        int num_dec = 0;
         for (int i = m; i > 0; i--) {
-            num += (int) (Math.pow(a, i-1) * Integer.parseInt(st.nextToken()));
+            num_dec += (int) (Math.pow(a, i-1) * Integer.parseInt(st.nextToken()));
         }
 
-        for (int i = m; i > 0; i--) {
-            System.out.print((int) (num / Math.pow(b, i-1)) + " ");
-            num %= (int) Math.pow(b, i-1);
+        int count = 0;
+        while (num_dec >= Math.pow(b, count)) {
+            count++;
+        }
+
+        for (int i = count; i > 0; i--) {
+            System.out.print((int) (num_dec / Math.pow(b, i-1)) + " ");
+            num_dec %= (int) Math.pow(b, i-1);
         }
     }
 }
