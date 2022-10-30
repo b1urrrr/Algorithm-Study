818e838a351955a3592d391eaa04547adcb67708
b1urrrr <b1urrr@naver.com>
2022-10-30 23:39:23 +0900

    [ADD] 수학은 체육과목 입니다 풀이 업로드

---
+++ 전채연/Mathematics (수학)/수학은_체육과목_입니다.java
@@ -0,0 +1,14 @@
+/*
+    백준 15894번 : 수학은 체육과목 입니다
+    - 문제 유형 : 수학, 사칙연산
+*/
+
+import java.util.Scanner;
+
+public class 수학은_체육과목_입니다 {
+    public static void main(String[] main) {
+        Scanner sc = new Scanner(System.in);
+        long square = sc.nextInt();
+        System.out.print(4 * square);
+    }
+}
