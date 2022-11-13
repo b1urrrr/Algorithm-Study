18404fd96d6beda539ebbef0b3542f2dc66b2a42
이가인 <heli1420@naver.com>
2022-11-13 23:54:17 +0900

    [ADD] WEEK38 문제업로드

---
+++ 이가인/Homework/WEEK37/4673.py
@@ -0,0 +1,17 @@
+def selfnum(maxnum):
+    int(num[0]) + int(num[1])
+''' str로 풀어보기 '''
+
+print(1)
+print(3)
+print(5)
+print(7)
+print(9)
+print(20)
+print(31)
+print(42)
+print(53)
+print(64)
+print(75)
+print(86)
+print(97)
