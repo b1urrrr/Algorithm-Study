07efd3940fb0d17208c7cf66ce89bff55bdd94ee
b1urrrr <b1urrr@naver.com>
2022-11-20 23:29:34 +0900

    [ADD] 전자레인지 풀이 업로드

---
+++ 전채연/Greedy (그리디)/전자레인지.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class 전자레인지 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int T = stdIn.nextInt();
+        int[] count = new int[3];
+
+        while (T > 0) {
+            if (T >= 300) {
+                T -= 300;
+                count[0]++;
+            } else if (T >= 60) {
+                T -= 60;
+                count[1]++;
+            } else if (T >= 10) {
+                T -= 10;
+                count[2]++;
+            } else {
+                System.out.println(-1);
+                return;
+            }
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print(count[i] + " ");
+        }
+        System.out.println();
+    }
+}
