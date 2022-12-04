bf3b15659fd66ecc5c7f16454b33158d1cc1c387
b1urrrr <b1urrr@naver.com>
2022-12-04 21:52:28 +0900

    [ADD] 인공지능 시계 풀이 업로드

---
+++ 전채연/Mathematics (수학)/인공지능_시계.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class 인공지능_시계 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int hr = stdIn.nextInt();
+        int min = stdIn.nextInt();
+        int sec = stdIn.nextInt();
+        int time = stdIn.nextInt();
+
+        hr += time / 3600;
+        time %= 3600;
+        min += time / 60;
+        time %= 60;
+        sec += time;
+
+        if (sec >= 60) {
+            min += sec / 60;
+            sec %= 60;
+        }
+        if (min >= 60) {
+            hr += min / 60;
+            min %= 60;
+        }
+        if (hr >= 24) {
+            hr %= 24;
+        }
+
+        System.out.printf("%d %d %d\n", hr, min, sec);
+    }
+}
