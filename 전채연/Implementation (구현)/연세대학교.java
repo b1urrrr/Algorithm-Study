0daf025ca4d851c677031284826d9786ea835998
b1urrrr <b1urrr@naver.com>
2022-12-11 23:58:39 +0900

    [ADD] 연세대학교 풀이 업로드

---
+++ 전채연/Implementation (구현)/연세대학교.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 연세대학교 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int n = stdIn.nextInt();
+        if (n == 0) System.out.println("YONSEI");
+        else System.out.println("Leading the Way to the Future");
+    }
+}
