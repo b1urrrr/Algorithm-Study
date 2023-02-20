1b96fb6dc5b8346b49deb849a2913e30b5f695b4
kinglim00 <tldtodthd52@naver.com>
2023-02-20 21:32:36 +0900

    [ADD] 50주차 백준 13164번

---
+++ 임서희/백준/50주차/13164번.py
@@ -0,0 +1,17 @@
+# 그리디 알고리즘, 정렬
+# 13164번: 행복 유치원
+# 차가 큰 애들은 아예 혼자 있게 해서 없애기. 즉 k-1의 합만 구하기
+
+# 틀렸다고 나와서 다시 생각해보기
+# 참고: https://yuna0125.tistory.com/46
+n, k = map(int, input().split())
+li = sorted(list(map(int, input().split())))
+
+li_cha = []
+#print(li)
+for i in range(1, n-1):
+    li_cha.append(abs(li[i]-li[i+1]))
+
+li_cha.sort()
+#print(li_cha)
+print(sum(li_cha[:k-1]))
