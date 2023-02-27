a825f34b5329b25fc75613695ed42f735b0315ee
kinglim00 <tldtodthd52@naver.com>
2023-02-27 23:18:35 +0900

    [ADD] 51주차 백준 24262번

---
+++ 임서희/백준/51주차/24262번.py
@@ -0,0 +1,11 @@
+# 시뮬레이션
+# 24262번: 알고리즘 수업- 알고리즘의 수행 시간 1
+def MenOfPassion(array, n):
+        i = n//2
+        return array[i]
+
+n = int(input())
+
+# 애초에 MenOfPassion 함수의 실행 횟수는 무조건 1회이고, 다항식의 최고차상은 상수이므로 0이 출력
+print(1)
+print(0)
