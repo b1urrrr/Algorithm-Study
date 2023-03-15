6fb4cb3e6945693ef0d1d3101475867db1945c80
kinglim00 <tldtodthd52@naver.com>
2023-03-15 23:12:16 +0900

    [MOD] 53주차 백준 16953번 코드 수정

--- 임서희/백준/53주차/16953번.py
+++ 임서희/백준/53주차/16953번.py
@@ -2,9 +2,22 @@
 # 16953번: A → B
 
 # 푸는 중
-a, b = map(int, input().split())
-res = 0
-while True:
-    if res >= b:
+# 코드 참고: https://my-coding-notes.tistory.com/210
+# BFS 이용하기
+from collections import deque
+a,b = map(int,input().split())
+q = deque()
+q.append((a,1))
+r = 0
+
+while(q):
+    n,t = q.popleft()
+    if n > b:
+        continue
+    if n == b:
+        print(t)
         break
-    
+    q.append((int(str(n)+"1"),t+1))
+    q.append((n*2,t+1))
+else:
+    print(-1)
