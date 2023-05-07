bb7552ec06ba26cf976c042da9d18a39a74225fe
kinglim00 <tldtodthd52@naver.com>
2023-05-07 22:02:38 +0900

    [ADD] 58주차 백준 문제 풀이

---
+++ 임서희/백준/58주차/2745번.py
@@ -0,0 +1,5 @@
+# 구현
+# 2745번: 진법 변환
+
+n, b = input().split()
+print(int(n,int(b))) # int 함수로 진법을 정할 수 있어서 n진법을 적용한 b의 값 출력
---
+++ 임서희/백준/58주차/5585번.py
@@ -0,0 +1,12 @@
+# 그리디 알고리즘
+# 5585번: 거스름돈
+
+n = int(input())
+moneys = [500, 100, 50, 10, 5, 1]
+n = 1000 - n
+res = 0
+for money in moneys:
+    if n >= money:
+        res += n//money
+        n = n%money
+print(res)
