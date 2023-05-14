bb58da30e08b13b8472f83343243afb632f2c482
b1urrrr <b1urrr@naver.com>
2023-05-15 02:06:55 +0900

    [ADD] 직사각형 풀이 업로드

---
+++ 전채연/Implementation (구현)/직사각형.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class 직사각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A * B);
+    }
+}
