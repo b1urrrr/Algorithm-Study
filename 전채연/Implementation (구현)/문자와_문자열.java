8e5792dc71f41dcaba7018b8a4b4eabe843c93a6
b1urrrr <b1urrr@naver.com>
2023-05-15 02:05:16 +0900

    [ADD] 문자와 문자열 풀이 업로드

---
+++ 전채연/Implementation (구현)/문자와_문자열.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class 문자와_문자열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.println(S.charAt(i - 1));
+    }
+}
