05002179875ad80b80710946e7442f783ab23ec5
MinYeongPark <yingo24655@naver.com>
2023-05-28 20:01:14 +0900

    [ADD] 61주차-5 백준 15894번 풀이

---
+++ 박민영/Math (수학)/백준/15894.java
@@ -0,0 +1,20 @@
+/*
+ * @author Minyeong Park
+ * @date 2023.05.28.
+ * 수학은 체육과목 입니다
+ * 문제 링크 : https://www.acmicpc.net/problem/15894
+ */
+
+
+import java.io.*;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long a = Integer.parseInt(br.readLine());
+        System.out.println(a * 4);
+    }
+}
