fb56d2aa5aeb9de5b3e2cff6999e5037341234fc
b1urrrr <b1urrr@naver.com>
2023-10-24 21:48:51 +0900

    [ADD/#600] 명제 증명 풀이 업로드 (오답)

---
+++ 전채연/Floyd-Warshall (플로이드-워셜)/명제_증명.java
@@ -0,0 +1,48 @@
+/*
+    백준 2224번 : 명제 증명
+    - 문제 유형 : 플로이드-워셜
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class 명제_증명 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine()); // 명제의 개수 N 입력
+        int alphabetCnt = 'z' - 'A' + 1;
+        boolean[][] graph = new boolean[alphabetCnt][alphabetCnt];
+
+        for (int i = 0; i < alphabetCnt; i++) graph[i][i] = true;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int assumption = st.nextToken().charAt(0) - 'A';
+            st.nextToken();
+            int conclusion = st.nextToken().charAt(0) - 'A';
+            graph[assumption][conclusion] = true;
+        }
+
+        int count = 0;
+        for (int a = 0; a < alphabetCnt; a++) {
+            for (int b = 0; b < alphabetCnt; b++) {
+                for (int i = 0; i < alphabetCnt; i++) {
+                    if (a == b || b == i || a == i || graph[a][b]) continue;
+                    if (graph[a][i] && graph[i][b]) graph[a][b] = true;
+                }
+            }
+        }
+
+        for (int a = 0; a < alphabetCnt; a++) {
+            for (int b = 0; b < alphabetCnt; b++) {
+                if (a == b || !graph[a][b]) continue;
+                sb.append((char) (a + 'A') + " => " + (char) (b + 'A') + "\n");
+                count++;
+            }
+        }
+        sb.insert(0, count + "\n");
+
+        System.out.print(sb);
+    }
+}
